5cdc7f65c84cb84387ca689ebe3db8a651d28876
Pepun Barua <pepunbarua@gmail.com>
2023-09-14 22:19:59 +0600

    Create simple_calculator.php

---
+++ module-1/simple_calculator.php
@@ -0,0 +1,23 @@
+<?php
+
+$a = 8;
+$b = 11;
+
+// Addition
+$addi = $a+$b;
+echo "Addition {$addi}";
+echo PHP_EOL;
+
+// Subtraction
+$subs = $a-$b;
+echo "Subtraction {$subs}";
+echo PHP_EOL;
+
+// Multiplication
+$multi = $a * $b;
+echo "Multiplication {$multi}";
+echo PHP_EOL;
+
+// Division
+$divi = $a / $b;
+echo "Division {$divi}";
