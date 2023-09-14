cb83ec86e590c34380380d4acd9ecc2c99616534
Pepun Barua <pepunbarua@gmail.com>
2023-09-14 20:55:35 +0600

    Create personal_info.php

---
+++ module-1/personal_info.php
@@ -0,0 +1,13 @@
+<?php<?php
+
+
+$name = "Pepun Barua";
+$age = 32;
+$country = "Bangladesh";
+$designation = "Owner & CEO";
+$companyName = "DEVSWIRE";
+
+
+
+echo "Hello There, My name is {$name}. My age {$age}, I live in {$country}. Currently I own a business, name {$companyName}. My currenty Position is {$designation}";
+
