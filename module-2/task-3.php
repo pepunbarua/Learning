e79c393b8267f45bb873495b234283c0a76ad6c8
Pepun Barua <pepunbarua@gmail.com>
2023-09-20 21:31:22 +0600

    Update task-3.php

--- module-2/task-3.php
+++ module-2/task-3.php
@@ -1,1 +1,21 @@
+<?php
 
+/*
+Task 3: Break on Condition
+Write a PHP program that calculates and prints the first 10 Fibonacci numbers. But, if a
+Fibonacci number is greater than 100, break out of the loop using the break statement.
+*/
+
+$a = 5;
+$b = 7;
+$c = 4;
+
+for($i=0;$i<10;$i++):
+    if($a>100):
+        break;
+    endif;
+    echo "{$a}, ";
+    $a = $b;
+    $b = $c;
+    $c = $a+$b;
+endfor;
